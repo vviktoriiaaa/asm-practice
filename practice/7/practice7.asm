f54a0b13f6f10a8a3913c6fb97c29932016553d6
vviktoriiaaa <viktoriia.moskva@e-u.edu.ua>
2026-04-06 12:48:24 +0300

    Практична 7: масив у пам’яті, заповнення, пошук мін/макс та їх індексів (NASM, int 0x80)

--- practice/7/practice7.asm
+++ practice/7/practice7.asm
@@ -6,31 +6,193 @@ BITS 32
 GLOBAL _start
 
 SECTION .data
-prompt db "practice7: see README.md", 10
-prompt_len equ $-prompt
+    prompt db "Enter n (5..50): ", 0
+    prompt_len equ $-prompt
+
+    msg_min db 10, "min = ", 0
+    len_min equ $-msg_min
+
+    msg_max db 10, "max = ", 0
+    len_max equ $-msg_max
+
+    msg_idx db ", index = ", 0
+    len_idx equ $-msg_idx
+
+    space db " "
 
 SECTION .bss
-buf resb 256
+    buf resb 256
+    arr resd 50      ; memory
+    n resd 1
+    tmp resb 16
 
 SECTION .text
 _start:
-    ; I/O: write prompt
-    mov eax, 4          ; sys_write
-    mov ebx, 1          ; stdout
+
+; ---------------- I/O ----------------
+    mov eax, 4
+    mov ebx, 1
     mov ecx, prompt
     mov edx, prompt_len
     int 0x80
 
-    ; I/O: read line (optional in skeleton)
-    mov eax, 3          ; sys_read
-    mov ebx, 0          ; stdin
+    mov eax, 3
+    mov ebx, 0
     mov ecx, buf
     mov edx, 255
     int 0x80
 
-    ; logic: TODO implement task logic according to README.md
+; ---------------- parse ----------------
+    mov esi, buf
+    xor eax, eax
 
-    ; exit
-    mov eax, 1          ; sys_exit
+parse_loop:
+    mov bl, [esi]
+    cmp bl, 10
+    je parse_done
+    sub bl, '0'
+    imul eax, eax, 10
+    add eax, ebx
+    inc esi
+    jmp parse_loop
+
+parse_done:
+    mov [n], eax
+
+; ---------------- logic ----------------
+    cmp eax, 5
+    jl exit
+    cmp eax, 50
+    jg exit
+
+; ---------------- loops (fill array) ----------------
+    xor ecx, ecx
+
+fill_loop:
+    mov eax, ecx
+    imul eax, eax      ; i*i
+    mov ebx, ecx
+    imul ebx, 3
+    sub eax, ebx
+    add eax, 7         ; math
+
+    mov [arr + ecx*4], eax   ; memory
+
+    inc ecx
+    cmp ecx, [n]
+    jl fill_loop
+
+; ---------------- loops (print array) ----------------
+    xor ecx, ecx
+
+print_loop:
+    mov eax, [arr + ecx*4]
+    push ecx
+    call print_int
+    pop ecx
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, space
+    mov edx, 1
+    int 0x80
+
+    inc ecx
+    cmp ecx, [n]
+    jl print_loop
+
+; ---------------- logic (min/max) ----------------
+    mov eax, [arr]
+    mov ebx, eax    ; min
+    mov edx, eax    ; max
+    xor esi, esi    ; min idx
+    xor edi, edi    ; max idx
+
+    mov ecx, 1
+
+find_loop:
+    mov eax, [arr + ecx*4]
+
+    cmp eax, ebx
+    jge skip_min
+    mov ebx, eax
+    mov esi, ecx
+
+skip_min:
+    cmp eax, edx
+    jle skip_max
+    mov edx, eax
+    mov edi, ecx
+
+skip_max:
+    inc ecx
+    cmp ecx, [n]
+    jl find_loop
+
+; ---------------- output min ----------------
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg_min
+    mov edx, len_min
+    int 0x80
+
+    mov eax, ebx
+    call print_int
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg_idx
+    mov edx, len_idx
+    int 0x80
+
+    mov eax, esi
+    call print_int
+
+; ---------------- output max ----------------
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg_max
+    mov edx, len_max
+    int 0x80
+
+    mov eax, edx
+    call print_int
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg_idx
+    mov edx, len_idx
+    int 0x80
+
+    mov eax, edi
+    call print_int
+
+; ---------------- exit ----------------
+exit:
+    mov eax, 1
     xor ebx, ebx
     int 0x80
+
+; ---------------- print_int ----------------
+print_int:
+    mov ecx, tmp + 15
+    mov byte [ecx], 0
+    mov ebx, 10
+
+convert:
+    dec ecx
+    xor edx, edx
+    div ebx
+    add dl, '0'
+    mov [ecx], dl
+    test eax, eax
+    jnz convert
+
+    mov eax, 4
+    mov ebx, 1
+    mov edx, tmp + 16
+    sub edx, ecx
+    mov esi, ecx
+    mov ecx, esi
+    int 0x80
+    ret
